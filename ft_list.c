61d30bdca1a5dc961fee58b9d0ee01363a6fd1a6
Laura Garcia Arch <laugarci@car6s2.42barcelona.com>
2023-04-15 18:37:15 +0200

    push_swap

--- ft_list.c
+++ ft_list.c
@@ -1,16 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_list.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/14 10:39:01 by laugarci          #+#    #+#             */
-/*   Updated: 2023/04/15 18:28:23 by laugarci         ###   ########.fr       */
+/*   Created: 2023/04/15 18:34:51 by laugarci          #+#    #+#             */
+/*   Updated: 2023/04/15 18:36:04 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (0);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
 
 t_list	*ft_lstnew(int val)
 {
