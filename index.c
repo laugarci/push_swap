1f387a19507c326cae5751c99308821358db7d11
Laura Garcia Arch <laugarci@car5s2.42barcelona.com>
2023-04-27 07:46:55 +0200

    push_swap

--- index.c
+++ index.c
@@ -6,7 +6,7 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:54:55 by laugarci          #+#    #+#             */
-/*   Updated: 2023/04/26 18:44:46 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/04/27 07:46:21 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int	index_is_sort(t_list *stack_a, int count)
 			i++;
 		stack_a = stack_a->next;
 	}
-	printf("i es %d\n", i);
-	printf("count es %d\n", count);
 	if (i != count)
 		return (1);
 	return (0);
@@ -38,10 +36,14 @@ int	find_min_num(t_list *stack_a)
 	
 	min_int = stack_a;
 	current = stack_a;
+	while (min_int->index != -1)
+		min_int = min_int->next;
 	while (current != NULL)
 	{
-		if (current->index == -1 && current->val < min_int->val)
+		if (current->index == -1 && current->val <= min_int->val)
+		{
 			min_int = current;
+		}
 		current = current->next;
 	}
 	return (min_int->val);
@@ -72,12 +74,11 @@ t_list	*ft_index(t_list *stack_a, int count)
 	int min;
 	int max;
 	int index;
+	
 
 	min = find_min_num(stack_a);
 	index = ft_find_index(stack_a);
 	aux = stack_a;
-	printf("min %d\n", min);
-	printf("index %d\n", index);
 	if (index != 0)
 		index = index + 1;
 	while (aux != NULL)
@@ -94,21 +95,13 @@ t_list	*ft_index(t_list *stack_a, int count)
 		aux = aux->next;
 	}
 	aux = stack_a;
-	printf("indice\n");
-	printf("%d\n", aux->index);
-	printf("%d\n", aux->next->index);
-	printf("%d\n", aux->next->next->index);
-	printf("%d\n", aux->next->next->next->index);
-	printf("%d\n", aux->next->next->next->next->index);
-	printf("%d\n", aux->next->next->next->next->next->index);
 	if ((index_is_sort(aux, count)) == 1)
 	{
-		write(1, "hola\n", 5);
 		ft_index(aux, count);
 	}
 	else
 	{
-		printf("ok\n");
 		return (aux);
 	}
+	return (aux);
 }
