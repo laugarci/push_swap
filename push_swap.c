9b59cfd3d5cf45b37f95a4191f622571ac8b4b68
Laura Garcia Arch <laugarci@car5s2.42barcelona.com>
2023-05-08 19:38:37 +0200

    push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,26 @@
 /*   By: laugarci <laugarci@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 16:05:44 by laugarci          #+#    #+#             */
-/*   Updated: 2023/05/02 16:38:54 by laugarci         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:37:40 by laugarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_free(t_list *stack)
+{
+	t_list *aux;
+
+	aux = stack;
+	while(aux != NULL)
+	{
+		printf("entra\n");
+		stack = aux;
+		aux = stack->next;
+		free(stack);
+	}
+	stack = NULL;
+}
 
 t_list	*ft_init_index(t_list *stack_a)
 {
@@ -70,11 +85,13 @@ int	ft_check_nums(char **str)
 		while (str[i][j])
 		{
 			if (str[i][j] >= '0' && str[i][j] <= '9' ||
-				str[i][j] == '-' && str[i][j + 1] >= '0' && str[i][j] <= '9')
-				j++;
+				str[i][j] == '-' && str[i][j + 1] >= '0' && str[i][j + 1] <= '9')
+			{
+					j++;
+			}
 			else
 			{
-				write(1, "Error\n", 6);
+				write(2, "Error\n", 6);
 				return (1);
 			}
 		}
@@ -92,7 +109,7 @@ int	main(int ac, char **av)
 
 	if (ac < 2)
 	{
-		write(1, "Error\n", 6);
+		write(2, "Error\n", 6);
 		return (0);
 	}
 	count = ac - 1;
@@ -109,7 +126,7 @@ int	main(int ac, char **av)
 	stack_a = ft_copy_stack(stack_a, av);
 	if (check_dup(stack_a) == 1)
 	{
-		write(1, "Error\n", 6);
+		write(2, "Error\n", 6);
 		return (0);
 	}
 	if (ft_stack_is_sort(stack_a, count) == 1)
@@ -123,5 +140,7 @@ int	main(int ac, char **av)
 		else
 			stack_a = ft_index_order(stack_a, stack_b);
 	}
+	ft_free(stack_a);
+	ft_free(stack_b);
 	return (0);
 }
